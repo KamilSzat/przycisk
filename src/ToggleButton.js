bd22eb54fb12dbde8a959ec5f693ee3396c2ca32
KamilSzat <uT_3776@zsp3zamosc.pl>
2024-11-13 09:50:22 +0100

    przycisk zmany tła

--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import './ToggleButton.css';
 
 export default function ToggleButton() {
-  const [isToggled, setIsToggled] = useState(true);
-  const handleChange = () => {
-    setIsToggled(!isToggled);
-  };
-  return(
-
+    const [isToggled, setIsToggled] = useState(false);
+    const handleChange = () => {
+        setIsToggled(!isToggled);
+    };
+    return(
         <button onClick={handleChange} className={`toggle-button ${isToggled ? 'on' :'off'}`}>
-          {isToggled ? 'Tryb jasny' : 'Tryb ciemny'}
+            {isToggled ? 'ON' : 'OFF'}
         </button>
-  );
+    );
 }
